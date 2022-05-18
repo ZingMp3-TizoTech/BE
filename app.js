d27d55b7400a30e7409c0ac9cceb7cae02333dde
QuangDung <dung05042000@gmail.com>
2022-05-18 11:11:01 +0700

    fixed heroku

--- app.js
+++ app.js
@@ -31,11 +31,8 @@ app.use(function (req, res, next) {
 });
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-
 app.use(Router);
-
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
-    
+app.listen(PORT, () => { 
   console.log(`Server is running on port ${PORT}`);
 });
