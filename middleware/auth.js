85ac715e385ed2f6f1bd3a38e48be34ede0e5fbb
QuangDung <dung05042000@gmail.com>
2022-05-27 08:58:28 +0700

    resete deploy

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,6 @@ const RepositoryRole = require('../Models/role')
 require("dotenv").config();
 const authenToken = async (req, res, next) => {
     const token = await req.header('Authorization').replace('Bearer ', '')
-    //if(req.header!=null){res.status(401).send({ error: 'Token null' })}
     if (!token) res.status(401).send({ error: 'Not authorized to access this resource' })
     const data = jwt.verify(token, process.env.JWT_KEY)
     console.log(data);  
